d04675d7cd9c16b7a0137023a8d0356c0a39a1d7
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2021-04-14 15:32:31 -0300

    Criando movimento da cobra

--- script.js
+++ script.js
@@ -17,13 +17,30 @@ function criarBG(){
 function criarCobra(){
     for(i=0; i<snake.length; i++){
         context.fillStyle = "green";
-        context.fillRect(snake[i].x, snaek[i].y, box, box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 function iniciarJogo(){
     criarBG();
     criarCobra();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
 }
 
 let jogo = setInterval(iniciarJogo, 100);
